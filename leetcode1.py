952c46055ba4b18a28f66cffe50d15fbd9497c35
Igor Djukic <idjukic91@gmail.com>
2024-03-02 11:12:55 +0100

    Finished LeetCode1

---
+++ leetcode1.py
@@ -0,0 +1,20 @@
+# Give an an array of integers (nums) [1,2,3,4,5] and a integer (target).
+# Return indices of the two numbers so that they add up to target
+# Each input has only one solution, can't use same element twice.
+
+#We look through the array
+input = [3,2,4]
+target = 6
+
+def TwoSum(input):
+    #loop through
+    for index, item1 in enumerate(input):
+        #take item as base, and loop trough remaining
+        for sindex, item2 in enumerate(input):
+            #if sindex = index continue
+            if index == sindex: continue
+            #equation adds up
+            if item1 + item2 == target: 
+                return([index,sindex])
+
+print(TwoSum(input))
