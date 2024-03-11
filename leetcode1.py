d44c07b798687cbf6eb389d25edcd11e6ebdc3cb
Igor Djukic <idjukic91@gmail.com>
2024-03-11 17:06:47 +0100

    adjusted leetcode 1

--- leetcode1.py
+++ leetcode1.py
@@ -3,18 +3,20 @@
 # Each input has only one solution, can't use same element twice.
 
 #We look through the array
+
+class Solution:
+    def TwoSum(self, input: list[int], target: int):
+        #loop through
+        for index, item1 in enumerate(input):
+            #take item as base, and loop trough remaining
+            for sindex, item2 in enumerate(input):
+                #if sindex = index continue
+                if index == sindex: continue
+                #equation adds up
+                if item1 + item2 == target: 
+                    return([index,sindex])
+
+Sol = Solution()
 input = [3,2,4]
 target = 6
-
-def TwoSum(input):
-    #loop through
-    for index, item1 in enumerate(input):
-        #take item as base, and loop trough remaining
-        for sindex, item2 in enumerate(input):
-            #if sindex = index continue
-            if index == sindex: continue
-            #equation adds up
-            if item1 + item2 == target: 
-                return([index,sindex])
-
-print(TwoSum(input))
+print(Sol.TwoSum(input,target))
